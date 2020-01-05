854805aaf4bc44117c787850c4216476e6c867a3
Julien Vier <vier.julien@googlemail.com>
2020-01-05 11:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-// function happyHolidaysTo(name) {
-//   return `Happy holidays, ${name}!`;
-// }
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
 
-function happyHolidaysTo (holiday, name) {
+function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
